04f98fb84a0eb42e898fed1308d35eeee3ce156b
j7tristan <j7_tristan@outlook.com>
2026-04-01 18:11:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 st.title("🥤 Customize Your Smoothie! 🥤")
 st.write("Choose the fruits you want in your custom Smoothie!")
@@ -7,8 +10,7 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select("FRUIT_NAME").collect()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME")).collect()
 fruit_list = [row["FRUIT_NAME"] for row in my_dataframe]
 
 ingredients_list = st.multiselect(
@@ -17,8 +19,9 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
+ingredients_string = ''
+
 if ingredients_list:
-    ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string = ingredients_string + fruit_chosen + ' '
 
